a310a80a7d6578ac88a537f19c0383c57d3cd2f7
Dhyey <dhyeymdeveloper@gmail.com>
2022-08-22 22:56:56 +0530

    Bottom Details Order Completed

--- Fashon/static/fashon/js/order_styling.js
+++ Fashon/static/fashon/js/order_styling.js
@@ -1,10 +1,14 @@
-// Hiding Customer Details Starts
 let userArrow = document.getElementById('userArrow');
 let userDetails = document.getElementById('userDetails');
 
 let topArrow = document.getElementById('topArrow');
 let topGrandParent = document.getElementById('topGrandParent');
 
+let bottomArrow = document.getElementById('bottomArrow');
+let bottomGrandParent = document.getElementById('bottomGrandParent');
+
+
+// Hiding Customer Details Starts
 function showHideUser(){
     if (userArrow.className.includes('active') == false){
         userDetails.style.display = "none"
@@ -13,6 +17,8 @@ function showHideUser(){
         userDetails.style.display = "block"
         topArrow.classList.remove('active')
         topGrandParent.style.display = "none"
+        bottomGrandParent.style.display = "none"
+        bottomArrow.classList.remove('active')
     }
 }
 // Hiding Customer Details Ends
@@ -28,6 +34,8 @@ function showHideTop(){
         topGrandParent.style.display = "block"
         userArrow.classList.remove('active')
         userDetails.style.display = "none"
+        bottomGrandParent.style.display = "none"
+        bottomArrow.classList.remove('active')
     }
 }
 
@@ -40,6 +48,37 @@ function showHideInsert() {
     }
     else if (arrow.className.includes('active') == true){
         insertOption.style.display = "block"
+    }
+}
+// Toggling Options Insert Ends
+// Hiding All Top Details Ends
+
+
+
+// Hiding All Top Details Starts
+function showHideBottom(){
+
+    if (bottomArrow.className.includes('active') == false){
+        bottomGrandParent.style.display = "none"
+    }
+    else if (bottomArrow.className.includes('active') == true){
+        bottomGrandParent.style.display = "block"
+        userArrow.classList.remove('active')
+        userDetails.style.display = "none"
+        topArrow.classList.remove('active')
+        topGrandParent.style.display = "none"
+    }
+}
+
+// Toggling Options Insert Starts
+function showHideBottomInsert() {
+    let insertBottomOption = document.getElementById('insertBottomOption');
+    let bottomInsertArrow = document.getElementById('bottomInsertArrow');
+    if (bottomInsertArrow.className.includes('active') == false){
+        insertBottomOption.style.display = "none"
+    }
+    else if (bottomInsertArrow.className.includes('active') == true){
+        insertBottomOption.style.display = "block"
     }
 }
 // Toggling Options Insert Ends
